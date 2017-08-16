8916900861e45d4f6bda21ed65699b0fa22103f6
David MacDonald <davidjohnmacdonald@gmail.com>
2017-08-15 23:34:10 -0400

    Preps for dot-org

--- airquote.php
+++ airquote.php
@@ -1,16 +1,17 @@
 <?php
 /*
 Plugin Name: So-Called Air Quotes
-Plugin URI: http://davidmacdonaldmusic.com
-Description: This plugin allows you to show "air quote" icons in your site. This uses embeded fonts to match your site's built-in styles as easily as possible; but, it may not work in some older browsers. To use, simply place text inside the aq shortcode like [aq]this[/aq]. You can add a special style attribute for [aq style=open]open air quote icons[/aq] like this.
+Description: This plugin allows you to show "air quote" icons in your site.
 Author: David MacDonald
 Version: 0.1
 Author URI: https://leftuseless.net
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
 */
 
 
 // Add a span to style
-function aq_quote_this($atts, $content=null) {
+function aq_quote_add_this($atts, $content=null) {
 	extract(shortcode_atts(array(
 		'style' => 'filled',
 	), $atts));
@@ -24,12 +25,12 @@ function aq_quote_this($atts, $content=null) {
 }
 
 // register the shortcode
-function register_shortcodes(){
-	add_shortcode('aq','aq_quote_this');
+function aq_quote_register_shortcodes(){
+	add_shortcode('aq','aq_quote_add_this');
 }
 
 // add shortcodes
-add_action( 'init', 'register_shortcodes' );
+add_action( 'init', 'aq_quote_register_shortcodes' );
 
 // allow shortcodes in widgets, comments, and excerpts as well!
 add_filter( 'widget_text', 'do_shortcode' );
@@ -37,11 +38,11 @@ add_filter( 'comment_text', 'do_shortcode' );
 add_filter( 'the_excerpt', 'do_shortcode');
 
 // Put the style in to do something with the special spans
-function add_aq_styles() {
+function aq_quote_add_aq_styles() {
 	wp_register_style( 'prefix-style', plugins_url('/assets/style.css', __FILE__) );
 	wp_enqueue_style( 'prefix-style' );
 }
 
-add_action( 'wp_enqueue_scripts', 'add_aq_styles' );
+add_action( 'wp_enqueue_scripts', 'aq_quote_add_aq_styles' );
 
 ?>
